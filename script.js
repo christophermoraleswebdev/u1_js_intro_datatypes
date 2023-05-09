e72e9fe0e48f64c90cbf0bc0cae0cf1f6548e25e
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-05-09 10:46:59 -0500

    end of data types

---
+++ script.js
@@ -0,0 +1,18 @@
+// Command + / will allow for us to make a comment
+// console.log('hello world');
+
+// Math Operators + - * /
+// exponents ** 
+// modulus - gives the remainder of a division equation 
+// % => modulus 
+// console.log(482.33145 / 2.31851)
+
+
+// Nesting our operations
+// console.log(Math.ceil(Math.sqrt(31)))
+
+
+// Strings -> words, sentences, and characters
+// We can use '' "" ``
+
+console.log('hello world'.toUpperCase())
